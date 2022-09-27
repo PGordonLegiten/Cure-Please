80cae28c5b62f4890f4d22a1a7dfb2959af6b988
lafilnemi <lafilnemi@gmail.com>
2022-09-27 13:41:34 +0200

    mini fixes

--- OptionsForm.Designer.cs
+++ OptionsForm.Designer.cs
@@ -8060,7 +8060,7 @@
             this.tabControl1.Size = new System.Drawing.Size(814, 587);
             this.tabControl1.TabIndex = 0;
             // 
-            // Form2
+            // OptionsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -8072,7 +8072,7 @@
             this.Controls.Add(this.button4);
             this.Controls.Add(this.tabControl1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "Form2";
+            this.Name = "OptionsForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = " ";
             this.TopMost = true;
